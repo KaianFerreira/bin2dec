e783ddac656e49264fb395fac2d0ffed31cf0e89
Kaian Ferreira dos Santos <=>
2021-03-06 20:57:18 -0300

    deploy

---
+++ precache-manifest.52b640325d5c2cd680ff82a11ba591d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f848ee20a80de645ce9f",
+    "url": "/bin2dec/css/app.5be7aafd.css"
+  },
+  {
+    "revision": "d3ea64da0a3a7f9805daaea0770ed613",
+    "url": "/bin2dec/index.html"
+  },
+  {
+    "revision": "f848ee20a80de645ce9f",
+    "url": "/bin2dec/js/app.cd17f135.js"
+  },
+  {
+    "revision": "8bddbe08e5824309c984",
+    "url": "/bin2dec/js/chunk-vendors.4ea8cb9c.js"
+  },
+  {
+    "revision": "4b653b8816dc1422a5ef0d8646c804d5",
+    "url": "/bin2dec/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bin2dec/robots.txt"
+  }
+]);
